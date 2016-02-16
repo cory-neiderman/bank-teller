f373002ad68cf6b36ed4eba17181529d7e5ff471
Cory Neiderman <cory.neiderman@gmail.com>
2016-02-15 23:15:20 -0500

    completed junit tests

--- test/CheckingAccountTest.java
+++ test/CheckingAccountTest.java
@@ -4,15 +4,15 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class SavingsAccountTest {
+public class CheckingAccountTest {
 	
-	private SavingsAccount theAccount;
+	private CheckingAccount theAccount;
 	
 	@Before
 	public void setup(){
 		DollarAmount zeroDollars = new DollarAmount(0);
 		BankCustomer theCustomer = new BankCustomer("John Doe", "123 Main St.", "123-46-7890");
-		this.theAccount = new SavingsAccount(theCustomer, "123456789", zeroDollars);
+		this.theAccount = new CheckingAccount(theCustomer, "123456789", zeroDollars);
 	}
 
 	@Test
@@ -33,8 +33,8 @@ public class SavingsAccountTest {
 	
 	
 	@Test
-	public void withdraw_decreases_the_amount_balance_below_zero_so_there_is_no_transaction(){
-		DollarAmount amountToWithdraw = new DollarAmount(12345);
+	public void withdraw_decreases_the_amount_balance_below_negative_100_including_10_dollar_fee_so_there_is_no_transaction(){
+		DollarAmount amountToWithdraw = new DollarAmount(9200);
 		DollarAmount newBalance = theAccount.withdraw(amountToWithdraw);
 		DollarAmount newBalanceAfter = new DollarAmount(0);
 		Assert.assertEquals(newBalanceAfter, newBalance);
@@ -52,36 +52,24 @@ public class SavingsAccountTest {
 	}
 	
 	@Test
-	public void withdraw_decreases_the_amount_balance_and_takes_a_two_dollar_charge_when_below_150(){
+	public void withdraw_decreases_the_amount_balance_and_takes_a_ten_dollar_charge_when_between_0_and_negative_100(){
 		theAccount.deposit(new DollarAmount(50000));
-		DollarAmount amountToWithdraw = new DollarAmount(40000);
+		DollarAmount amountToWithdraw = new DollarAmount(51000);
 		DollarAmount newBalance = theAccount.withdraw(amountToWithdraw);
-		DollarAmount newBalanceAfter = new DollarAmount(9800);
+		DollarAmount newBalanceAfter = new DollarAmount(-2000);
 		Assert.assertEquals(newBalanceAfter, newBalance);
 		Assert.assertEquals(newBalanceAfter, theAccount.getBalance());
 	}
-	
-	@Test
-	public void transfer_deposits_amount_into_account_and_withdraws_from_another_account(){
-		DollarAmount amountToTransfer = new DollarAmount(14000);
-		BankCustomer transferCustomer = new BankCustomer("Joe Schmoe", "123 first St.", "111-46-7890");
-		SavingsAccount transferAccount = new SavingsAccount(transferCustomer, "1122334455", new DollarAmount(30000));
-		transferAccount.transfer(theAccount, amountToTransfer);
-		DollarAmount transferAccountNewBalance = new DollarAmount(16000);
-		Assert.assertEquals(transferAccountNewBalance, transferAccount.getBalance());
-		DollarAmount theAccountNewBalance = new DollarAmount(14000);
-		Assert.assertEquals(theAccountNewBalance, theAccount.getBalance());
-
-	}
 
 	@Test
-	public void getAccountType_returns_savings_account(){
-		Assert.assertEquals("Savings Account", theAccount.getAccountType());
+	public void getAccountType_returns_Checking_account(){
+		Assert.assertEquals("Checking Account", theAccount.getAccountType());
 	}
 	
 	@Test
 	public void getAccountNumber_returns_account_number(){
 		Assert.assertEquals("123456789", theAccount.getAccountNumber());
 	}
-}
 	
+
+}
--- test/DollarAmountTest.java
+++ test/DollarAmountTest.java
@@ -6,24 +6,136 @@ import org.junit.Test;
 public class DollarAmountTest {
 	
 	@Test
+	public void getCents_returns_the_number_of_cents(){
+		DollarAmount anAmount = new DollarAmount(1234);
+		long numberOfCents = anAmount.getCents();
+		Assert.assertEquals(34, numberOfCents);
+		
+		DollarAmount anotherAmount = new DollarAmount(105, 65);
+		long anotherNumberOfCents = anotherAmount.getCents();
+		Assert.assertEquals(65, anotherNumberOfCents);
+	}
+	
+	@Test
 	public void getDollars_returns_the_number_of_whole_dollars(){
 		DollarAmount anAmount = new DollarAmount(1234);
 		long numberOfDollars=anAmount.getDollars();
 		Assert.assertEquals(12, numberOfDollars);
+		
+		DollarAmount anotherAmount = new DollarAmount(105, 65);
+		long anotherNumberOfDollars = anotherAmount.getDollars();
+		Assert.assertEquals(105, anotherNumberOfDollars);
 	}
 
 	@Test
-	public void getCents_returns_the_number_of_cents(){
-		
-		DollarAmount anAmount = new DollarAmount(1234);
-		long numberOfCents = anAmount.getCents();
-		Assert.assertEquals(34, numberOfCents);
+	public void equals_returns_true_for_equal_amounts(){
+		DollarAmount firstAmount = new DollarAmount(1234);
+		DollarAmount secondAmount = new DollarAmount(1234);
+		Assert.assertTrue(firstAmount.equals(secondAmount));
 	}
 	
 	@Test
-	public void isGreaterThan_returns_true_for_lesser_amuonts(){
+	public void equals_returns_false_for_unequal_amounts(){
+		DollarAmount firstAmount = new DollarAmount(1234);
+		DollarAmount secondAmount = new DollarAmount(1233);
+		Assert.assertFalse(firstAmount.equals(secondAmount));
+	}
+	
+	@Test
+	public void isLessThan_returns_true_for_lesser_amuonts(){
+		DollarAmount firstAmount = new DollarAmount(1232);
+		DollarAmount secondAmount = new DollarAmount(1233);
+		Assert.assertTrue(firstAmount.isLessThan(secondAmount));
+	}
+	
+	@Test
+	public void isLessThan_returns_false_for_greater_amuonts(){
+		DollarAmount firstAmount = new DollarAmount(1232);
+		DollarAmount secondAmount = new DollarAmount(1233);
+		Assert.assertFalse(secondAmount.isLessThan(firstAmount));
+	}
+	
+	@Test
+	public void isGreaterThan_returns_true_for_greater_amuonts(){
 		DollarAmount firstAmount = new DollarAmount(1234);
 		DollarAmount secondAmount = new DollarAmount(1233);
 		Assert.assertTrue(firstAmount.isGreaterThan(secondAmount));
 	}
+	
+	@Test
+	public void isGreaterThan_returns_false_for_lesser_amuonts(){
+		DollarAmount firstAmount = new DollarAmount(1234);
+		DollarAmount secondAmount = new DollarAmount(1233);
+		Assert.assertFalse(secondAmount.isGreaterThan(firstAmount));
+	}
+	
+	@Test
+	public void isNegative_returns_true_for_negative_balance(){
+		DollarAmount firstAmount = new DollarAmount(-1234);
+		Assert.assertTrue(firstAmount.isNegative());
+	}
+	
+	@Test
+	public void isNegative_returns_false_for_nonnegative_balance(){
+		DollarAmount firstAmount = new DollarAmount(1234);
+		Assert.assertFalse(firstAmount.isNegative());
+		DollarAmount zeroAmount = new DollarAmount(0);
+		Assert.assertFalse(zeroAmount.isNegative());
+	}
+	
+	@Test
+	public void plus_returns_true_amount_if_equal_to_added_amounts(){
+		DollarAmount firstAmount = new DollarAmount(1234);
+		DollarAmount secondAmount = new DollarAmount(1233);
+		DollarAmount thirdAmount = new DollarAmount(2467);
+
+		Assert.assertTrue(firstAmount.plus(secondAmount).equals(thirdAmount));
+
+		
+	}
+	@Test
+	public void minus_returns_true_amount_if_equal_to_added_amounts(){
+		DollarAmount firstAmount = new DollarAmount(1234);
+		DollarAmount secondAmount = new DollarAmount(1233);
+		DollarAmount thirdAmount = new DollarAmount(1);
+		Assert.assertTrue(firstAmount.minus(secondAmount).equals(thirdAmount));
+	}
+	
+	@Test 
+	public void compareTo_returns_0_for_equal_amounts(){
+		DollarAmount firstAmount = new DollarAmount(1234);
+		DollarAmount secondAmount = new DollarAmount(1234);
+		Assert.assertEquals(0, firstAmount.compareTo(secondAmount));
+	}
+	
+	@Test
+	public void compareTo_returns_1_for_greater_amounts(){
+		DollarAmount firstAmount = new DollarAmount(1234);
+		DollarAmount secondAmount = new DollarAmount(1233);
+		Assert.assertEquals(1, firstAmount.compareTo(secondAmount));
+	}
+	
+	@Test
+	public void compareTo_returns_negative_1_for_lesser_amounts(){
+		DollarAmount firstAmount = new DollarAmount(1234);
+		DollarAmount secondAmount = new DollarAmount(1233);
+		Assert.assertEquals(-1, secondAmount.compareTo(firstAmount));
+	}
+	
+	@Test
+	public void toString_returns_DollarAmount_as_string(){
+		DollarAmount firstAmount = new DollarAmount(1234);
+		Assert.assertEquals("$12.34", firstAmount.toString());
+		DollarAmount otherAmount = new DollarAmount(1201);
+		Assert.assertEquals("$12.01", otherAmount.toString());
+
+	}
+	
+	@Test
+	public void hashCode_returns_proper_hash_code(){
+		DollarAmount firstAmount = new DollarAmount(1234);
+		int firstAmountInt = 1234;
+		Assert.assertEquals(firstAmountInt, firstAmount.hashCode());
+	}
+	
 }
